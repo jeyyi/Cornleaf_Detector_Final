877f25328feef4c200c315f95efca56b385d5f5f
Jayrhald Padilla <jayrhald.jp@gmail.com>
2023-07-25 15:57:04 +0800

    Fixed the parent-child routing for reactrouterv6

--- src/index.js
+++ src/index.js
@@ -9,44 +9,83 @@ import Navigation from "./Components/Navigation";
 import Feed from "./Feed";
 import ProfileFeed from "./ProfileFeed";
 import Login from "./Login";
-import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 import PrivateRoute from "./Components/ProtectedRoutes";
 
 const isUserAuthenticated = () => {
   // Replace this with your actual authentication check, e.g., check for a valid token in local storage
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem("authToken");
   // return !!token; // Return true if the token is present, false otherwise
-  return true
+  return true;
 };
-const PrivateWrapper = ({children}) => {
-  return isUserAuthenticated() ? children 
-  : <Navigate to="/login" />;
+const PrivateWrapper = ({ children }) => {
+  return isUserAuthenticated() ? children : <Navigate to="/login" />;
 };
 
 const LoginWrapper = () => {
-  return isUserAuthenticated() ? <Home /> : <Navigate to="/" replace/>
-}
+  return isUserAuthenticated() ? <Home /> : <Navigate to="/" replace />;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
+      <Routes>
         {/* Public routes */}
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<LoginWrapper><Login /></LoginWrapper>} />
+
+        <Route
+          path="/login"
+          element={
+            <LoginWrapper>
+              <Login />
+            </LoginWrapper>
+          }
+        />
 
         {/* Protected routes */}
-          <Route 
-            path="/" 
+        <Route
+          path="/"
+          element={
+            <PrivateWrapper>
+              <Navigation />
+            </PrivateWrapper>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route
+            path="/classify"
             element={
-            <PrivateWrapper><Home/></PrivateWrapper>
-          }/>
-          <Route path="/classify" element={<PrivateWrapper><App /></PrivateWrapper>} />
-          <Route path="/feed" element={<PrivateWrapper><Feed /></PrivateWrapper>} />
-          <Route path="/profilefeed" element={<PrivateWrapper><ProfileFeed /></PrivateWrapper>} />
-          {/* Add other protected routes here */}
+              <PrivateWrapper>
+                <App />
+              </PrivateWrapper>
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Route>
+        <Route
+          path="/feed"
+          element={
+            <PrivateWrapper>
+              <Feed />
+            </PrivateWrapper>
+          }
+        />
+        <Route
+          path="/profilefeed"
+          element={
+            <PrivateWrapper>
+              <ProfileFeed />
+            </PrivateWrapper>
+          }
+        />
+        {/* Add other protected routes here */}
         {/* </Route> */}
 
         {/* Not Found route */}
